0029edaa8ab17ff1e4a7e2ad15a99860096e0b02
tylerdn7 <24862863+tylerdn7@users.noreply.github.com>
2018-02-24 09:54:34 +0600

    Create hello_world.cpp

---
+++ hackerrank/hello_world.cpp
@@ -0,0 +1,27 @@
+/**
+** Hello World Program
+** Ahasanul Basher Hamza
+** Date: 24/02/18
+***/
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // Declare a variable named 'input_string' to hold our input.
+    string input_string; 
+    
+    // Read a full line of input from stdin (cin) and save it to our variable, input_string.
+    getline(cin, input_string); 
+    
+    // Print a string literal saying "Hello, World." to stdout using cout.
+    cout << "Hello, World." << endl;
+    cout << "Welcome to 30 Days of Code!" << endl;
+    // TODO: Write a line of code here that prints the contents of input_string to stdout.
+
+    return 0;
+}
