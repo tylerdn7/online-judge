67bb362339b89df4e8f5634096cde55b047a2a50
tylerdn7 <24862863+tylerdn7@users.noreply.github.com>
2018-02-26 22:34:33 +0600

    Create 1004.c

---
+++ urioj/1004.c
@@ -0,0 +1,16 @@
+/**
+** 1004 - Simple Product
+** Ahasanul Basher Hamza
+** Date: 3/6/16
+***/
+
+#include <stdio.h>
+
+int main() 
+{
+    int a,b;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    printf("PROD = %d\n",a*b);
+    return 0;
+}
