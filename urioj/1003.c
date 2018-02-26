5977462ff11e12e2654ca6dee3fe36c7e6f3e5da
tylerdn7 <24862863+tylerdn7@users.noreply.github.com>
2018-02-26 22:33:13 +0600

    Create 1003.c

---
+++ urioj/1003.c
@@ -0,0 +1,15 @@
+/**
+** 1003 - Simple Sum
+** Ahasanul Basher Hamza
+** Date: 3/6/16
+***/
+
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    printf("SOMA = %d\n",a+b);
+    return 0;
+}
