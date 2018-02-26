acb7a2fe23ccf1ff116b15dd80bf07df5cd42926
tylerdn7 <24862863+tylerdn7@users.noreply.github.com>
2018-02-26 22:31:22 +0600

    Create 1001.c

---
+++ urioj/1001.c
@@ -0,0 +1,16 @@
+/**
+** Extremely Basic Program
+** Ahasanul Basher Hamza
+** Date: 3/6/16
+***/
+#include <stdio.h>
+
+int main() {
+ 
+    int a,b,x;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    x = a + b;
+    printf("X = %d\n",x);
+    return 0;
+}
