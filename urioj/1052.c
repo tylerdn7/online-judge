c323b3bea485a58e6b573d9020dd60b6b5655df7
tylerdn7 <24862863+tylerdn7@users.noreply.github.com>
2018-03-02 20:56:17 +0600

    Create 1052.c

---
+++ urioj/1052.c
@@ -0,0 +1,57 @@
+/**
+** 1052 - Month
+** Ahasanul Basher Hamza
+** Date: 3/6/16
+***/
+
+#include <stdio.h>
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+    int i;
+    scanf("%d",&i);
+    if(i == 1)
+    {
+        printf("January\n");
+    }
+    if(i == 2)
+    {
+        printf("February\n");
+    }if(i == 3)
+    {
+        printf("March\n");
+    }if(i == 4)
+    {
+        printf("April\n");
+    }if(i == 5)
+    {
+        printf("May\n");
+    }if(i == 6)
+    {
+        printf("June\n");
+    }if(i == 7)
+    {
+        printf("July\n");
+    }if(i == 8)
+    {
+        printf("August\n");
+    }if(i == 9)
+    {
+        printf("September\n");
+    }if(i == 10)
+    {
+        printf("October\n");
+    }if(i == 11)
+    {
+        printf("November\n");
+    }if(i == 12)
+    {
+        printf("December\n");
+    }
+    return 0;
+}
