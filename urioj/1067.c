78c9a9f907fca01d68a934f47dcef8736c4de68c
tylerdn7 <24862863+tylerdn7@users.noreply.github.com>
2018-03-02 20:54:58 +0600

    Create 1067.c

---
+++ urioj/1067.c
@@ -0,0 +1,26 @@
+/**
+** 1067 - Odd Numbers
+** Ahasanul Basher Hamza
+** Date: 3/6/16
+***/
+
+#include <stdio.h>
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+    int i,j;
+    scanf("%d",&i);
+    for(j = 1;j <= i;j++)
+    {
+        if(j % 2 != 0)
+        {
+            printf("%d\n",j);
+        }
+    }
+    return 0;
+}
