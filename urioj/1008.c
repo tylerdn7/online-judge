6c6620d947dc75458e7054dd08f1b961ec051735
tylerdn7 <24862863+tylerdn7@users.noreply.github.com>
2018-03-02 20:55:43 +0600

    Create 1008.c

---
+++ urioj/1008.c
@@ -0,0 +1,21 @@
+/**
+** 1008 - Salary
+** Ahasanul Basher Hamza
+** Date: 3/6/16
+***/
+
+#include <stdio.h>
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+    int i,j;
+    float k;
+    scanf("%d %d %f",&i,&j,&k);
+    printf("NUMBER = %d\nSALARY = U$ %0.2f\n",i,(float)j*k);
+    return 0;
+}
