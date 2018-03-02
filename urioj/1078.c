a41111233b7940322641d28f64e4bccaf7c98331
tylerdn7 <24862863+tylerdn7@users.noreply.github.com>
2018-03-02 20:52:51 +0600

    Create 1078.c

---
+++ urioj/1078.c
@@ -0,0 +1,23 @@
+/**
+** 1078 - Multiplication Table
+** Ahasanul Basher Hamza
+** Date: 3/6/16
+***/
+
+#include <stdio.h>
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+    int n,i;
+    scanf("%d", &n);
+    for(i = 1;i <= 10;i++)
+    {
+        printf("%d x %d = %d\n",i,n,i*n);
+    }
+    return 0;
+}
