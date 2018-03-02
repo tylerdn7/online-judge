7c0ff46e25cc88b5fddeca4ded5cedd263ff8910
tylerdn7 <24862863+tylerdn7@users.noreply.github.com>
2018-03-02 21:01:17 +0600

    Create 1050.c

---
+++ urioj/1050.c
@@ -0,0 +1,56 @@
+/**
+** 1050 - DDD
+** Ahasanul Basher Hamza
+** Date: 3/7/16
+***/
+
+#include <stdio.h>
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+     int i;
+    scanf("%d",&i);
+    if(i == 11)
+    {
+        printf("Sao Paulo\n");
+    }
+    else if(i == 21)
+    {
+        printf("Rio de Janeiro\n");
+    }
+    else if(i == 32)
+    {
+        printf("Juiz de Fora\n");
+    }
+    else if(i == 19)
+    {
+        printf("Campinas\n");
+    }
+    else if(i == 27)
+    {
+        printf("Vitoria\n");
+    }
+    else if(i == 31)
+    {
+        printf("Belo Horizonte\n");
+    }
+    else if(i == 61)
+    {
+        printf("Brasilia\n");
+    }
+    else if(i == 71)
+    {
+        printf("Salvador\n");
+    }
+    else
+    {
+        printf("DDD nao cadastrado\n");
+    }
+
+    return 0;
+}
