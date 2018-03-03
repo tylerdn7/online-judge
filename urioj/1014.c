e6f969badb228b278413952b82701b729c5e4372
tylerdn7 <24862863+tylerdn7@users.noreply.github.com>
2018-03-03 23:46:20 +0600

    Create 1014.c

---
+++ urioj/1014.c
@@ -0,0 +1,24 @@
+/**
+** 1014 - Consumption
+** Ahasanul Basher Hamza
+** Date: 3/8/16
+***/
+
+#include <stdio.h>
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+    int a;
+    float b,i;
+    scanf("%d",&a);
+    scanf("%f",&b);
+    i = a /b;
+    printf("%0.3f km/l\n",i);
+    
+    return 0;
+}
