ec426edc5e19d1b076dd0248429574988bf17d38
tylerdn7 <24862863+tylerdn7@users.noreply.github.com>
2018-03-03 23:47:16 +0600

    Create 1010.c

---
+++ urioj/1010.c
@@ -0,0 +1,23 @@
+/**
+** 1010 - Simple Calculate
+** Ahasanul Basher Hamza
+** Date: 3/8/16
+***/
+
+#include <stdio.h>
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+    int a,b,d,e;
+    float i,c,f;
+    scanf("%d %d %f",&a,&b,&c);
+    scanf("%d %d %f",&d,&e,&f);
+    i = b*c + e*f;
+    printf("VALOR A PAGAR: R$ %0.2f\n",i);
+    return 0;
+}
