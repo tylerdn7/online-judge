9b8730a711daf7194847c2f7ad2fbac54260bb0d
tylerdn7 <24862863+tylerdn7@users.noreply.github.com>
2018-03-03 23:44:51 +0600

    Create 1002.c

---
+++ urioj/1002.c
@@ -0,0 +1,23 @@
+/**
+** 1002 - Area of a Circle
+** Ahasanul Basher Hamza
+** Date: 3/8/16
+***/
+
+#include <stdio.h>
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+    double area,r;
+    float pi = 3.14159;
+    scanf("%lf",&r);
+    area = (r*r) * 3.14159;
+    printf("A=%0.4lf\n",area);
+    
+    return 0;
+}
