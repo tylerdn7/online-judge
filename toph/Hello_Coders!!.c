a067b404748a0eb11e87140bdc14b795fe0bb588
tylerdn7 <24862863+tylerdn7@users.noreply.github.com>
2018-03-05 01:22:23 +0600

    Create Hello_Coders!!.c

---
+++ toph/Hello_Coders!!.c
@@ -0,0 +1,26 @@
+/**
+** Hello Coders!!
+** Ahasanul Basher Hamza
+** Date: 5/03/18
+***/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <math.h>
+#include <ctype.h>
+
+
+int main()
+{
+    int i,n,j;
+    scanf("%d", &n);
+    printf("Hello IUT");
+    for(i = 0;i < n;i++)
+    {
+        printf("!");
+    }
+    printf("\n");
+    return 0;
+}
