773b2f30597da7d53689c788125a2cc8727de3ed
tylerdn7 <24862863+tylerdn7@users.noreply.github.com>
2018-03-05 03:07:08 +0600

    Create 1.Data_Types.java

---
+++ hackerrank/30 Days of Code Challenges/1.Data_Types.java
@@ -0,0 +1,30 @@
+/**
+** Data Types
+** Ahasanul Basher Hamza
+** Date: 4/03/16
+***/
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+      public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+		
+        Scanner scan = new Scanner(System.in);
+            int i1 = scan.nextInt();
+            double d1 = scan.nextDouble();
+            scan.nextLine();
+            String p = scan.nextLine();
+            System.out.println(i+i1);
+            System.out.println(d+d1);
+            System.out.println(s+p);
+            scan.close();
+    	}
+
+}
