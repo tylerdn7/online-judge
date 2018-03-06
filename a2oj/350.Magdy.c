d2949acde75c0b82bbec1c32d0eb34dbd3221baa
tylerdn7 <24862863+tylerdn7@users.noreply.github.com>
2018-03-06 19:35:17 +0600

    Create 350.Magdy.c

---
+++ a2oj/350.Magdy.c
@@ -0,0 +1,26 @@
+/**
+** Magdy
+** Ahasanul Basher Hamza
+** Date: 5/29/16
+***/
+
+#include <stdio.h>
+
+int main()
+{
+    int s;
+    scanf("%d", &s);
+    if(s > 1)
+    {
+        printf("You Rocks Man\n");
+    }
+    else if(s < 1)
+    {
+        printf("Don't be lazy, it takes only few minutes, you can do it.\n");
+    }
+    else if(s == 1)
+    {
+        printf("Good Job\n");
+    }
+return 0;
+}
