ab2b4222f08b449f140c9e884ac94a96954cef46
tylerdn7 <24862863+tylerdn7@users.noreply.github.com>
2018-03-06 19:31:01 +0600

    Create 346.Add_them_twice.c

---
+++ a2oj/346.Add_them_twice.c
@@ -0,0 +1,15 @@
+/**
+** Add them twice
+** Ahasanul Basher Hamza
+** Date: 5/12/16
+***/
+
+#include <stdio.h>
+
+int main()
+{
+    float a,b,c;
+    scanf("%f %f %f", &a, &b, &c);
+    printf("%f\n",a+a+b+b+c+c);
+    return 0;
+}
