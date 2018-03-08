5614281987fb0aaa764436e6b03fd148299dcca3
tylerdn7 <24862863+tylerdn7@users.noreply.github.com>
2018-03-08 23:03:38 +0600

    Create 1017.c

---
+++ urioj/1017.c
@@ -0,0 +1,18 @@
+/**
+** 1017 - Fuel Spent
+** Ahasanul Basher Hamza
+** Date: 5/17/16
+***/
+
+#include <stdio.h>
+ 
+int main() 
+{
+    int a,b;
+    float c;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    c = (float) (a*b )/ 12;
+    printf("%0.3f\n",c);
+    return 0;
+}
