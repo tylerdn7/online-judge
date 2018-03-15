5214d0930d3ab199c8806795b0cb9fb44f1c80fe
tylerdn7 <24862863+tylerdn7@users.noreply.github.com>
2018-03-15 23:45:37 +0600

    Create 1864.c

---
+++ urioj/1864.c
@@ -0,0 +1,20 @@
+/**
+** 1864 - Our Days Are Never Coming Back
+** Ahasanul Basher Hamza
+** Date: 5/20/16
+***/
+
+#include <stdio.h>
+ 
+int main() 
+{
+    char s[35] = "LIFE IS NOT A PROBLEM TO BE SOLVED";
+    int i,n;
+    scanf("%d", &n);
+    for(i = 0;i < n;i++)
+    {
+        printf("%c",s[i]);
+    }
+    printf("\n");
+    return 0;
+}
