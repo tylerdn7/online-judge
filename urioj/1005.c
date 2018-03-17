2c0dc63cf230571913bb2283ca5ab6f51b2f1454
tylerdn7 <24862863+tylerdn7@users.noreply.github.com>
2018-03-17 22:50:51 +0600

    Create 1005.c

---
+++ urioj/1005.c
@@ -0,0 +1,21 @@
+/**
+** 1005 - Average 1
+** Ahasanul Basher Hamza
+** Date: 5/22/16
+***/
+
+#include <stdio.h>
+ 
+int main() 
+{
+    double a,b;
+    scanf("%lf", &a);
+    scanf("%lf", &b);
+    double media;
+    a = a * 3.5;
+    b = b * 7.5;
+    media = (a + b) / (3.5+7.5);
+    printf("MEDIA = %.5f\n",media);
+    return 0;
+    return 0;
+}
